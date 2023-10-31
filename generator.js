42595513bb65acef6976268866e9fdb28bc39651
Mboumela Elton <mboumelae@gmail.com>
2023-10-31 12:47:09 +0100

    package.json, main name modification

--- generator.js
+++ generator.js
@@ -5,7 +5,7 @@ const QRCode = require('qrcode');
   input : data (for exemple a text or an URL)
   output : image of QRCode or an error
  */
-function generateQRCode(data) {
+function QRCodeGenerator(data) {
   return new Promise((resolve, reject) => {
     QRCode.toDataURL(data, (error, url) => {
       if (error) {
@@ -17,4 +17,4 @@ function generateQRCode(data) {
   });
 }
 
-module.exports = { generateQRCode };
+module.exports = { QRCodeGenerator };
